6afb00eb731084333658133eab2ffd84daa53ec9
impliedchaos <crap@dharmadevil.com>
2021-01-27 15:12:46 -0500

    add 'reauth' convenience command

--- mopidy_ytmusic/command.py
+++ mopidy_ytmusic/command.py
@@ -8,6 +8,7 @@ class YTMusicCommand(commands.Command):
     def __init__(self):
         super().__init__()
         self.add_child("setup", SetupCommand())
+        self.add_child("reauth", ReSetupCommand())
 
 
 class SetupCommand(commands.Command):
@@ -44,4 +45,31 @@ class SetupCommand(commands.Command):
         print("   [ytmusic]")
         print("   enabled=true")
         print("   auth_json=" + str(path))
+        return 0
+
+
+class ReSetupCommand(commands.Command):
+    help = "Regenerate auth.json"
+
+    def run(self, args, config):
+        from ytmusicapi.ytmusic import YTMusic
+
+        path = config["ytmusic"]["auth_json"]
+        if not path:
+            logger.error('auth_json path not defined in config')
+            return 1
+        print('Updating credentials in  "' + str(path) + '"')
+        print(
+            "Open Youtube Music, open developer tools (F12), go to Network tab,"
+        )
+        print(
+            'right click on a POST request and choose "Copy request headers".'
+        )
+        print("Then paste (CTRL+SHIFT+V) them here and press CTRL+D.")
+        try:
+            print(YTMusic.setup(filepath=str(path)))
+        except Exception:
+            logger.exception("YTMusic setup failed")
+            return 1
+        print("Authentication JSON data saved to {}".format(str(path)))
         return 0
